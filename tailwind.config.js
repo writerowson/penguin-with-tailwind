6d3e06747141cc966a3815bad48834bd27796d19
coderowson <runurowson@gmail.com>
2022-01-22 18:10:46 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// < !-- for customized tailwind css - optional to use tailwind extention-- >
+
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
